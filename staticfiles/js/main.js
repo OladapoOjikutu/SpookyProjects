7c1acd8bd899830c6abc87f1b143f6150def9083
OladapoOjikutu <git-commit-address>
2022-11-15 02:49:03 +0000

    further ammendment

---
+++ staticfiles/js/main.js
@@ -0,0 +1,9 @@
+
+$('.testimonial-wrap').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+  });
